e823a53552659d0562467f158813d2f5f41ec4ab
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-04-11 19:23:32 +0530

    Done upto exercise 9 (Palindrome checker)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(num1, num2) {
+  return (num1 + num2);
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+	return (num1 - num2)
+};
+
+const sum = function(array) {
+	return array.reduce((total,num)=>total+=num,0);
+};
+
+const multiply = function(array) {
+  let multiplication = 1;
+  for (i = 0;i<array.length;i++){
+    multiplication = multiplication*array[i];
+  }
+  return multiplication;
+};
+
+const power = function(num,exp) {
+  return num ** exp;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0 || num === 1){
+    return 1;
+  }
+  let fact = 1;
+  for(let i = num;i>0;i--){
+    fact = fact*i;
+  }
+  return fact;	
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let processedString = inputString.toLowerCase().replace(/[^\w]/g, '');
+    let halfLength = processedString.length/2;
+    let maxIndex = processedString.length -1 ;
+    for(let i = 0; i<=halfLength;i++){
+        if(processedString[i] !== processedString[maxIndex-i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
