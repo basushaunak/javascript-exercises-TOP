e3df99554f47c225d712df9d991f01c4c29419d1
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-04-12 06:30:15 +0530

    exercise 12 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArray) {
+    let titleArray = booksArray.map((book)=>book.title);
+    return titleArray;
 };
 
 // Do not edit below this line
